852cee9901494980f45dcc18f19e26c6360da26d
Timothy <timothy.oxlee@kingsway.school.nz>
2024-06-18 20:31:45 +1200

    Working Light/Dark Mode
    I added a light and dark mode toggle which can be implemented throughout the project.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {NextUIProvider} from "@nextui-org/react";
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { ThemeSwitcher } from "./components/ThemeSwitcher";
 
 export default function Home() {
   return (
+<<<<<<< Updated upstream
     <main className="flex min-h-screen items-center">
       <h1>Schlangen</h1>
+=======
+    <main className="text-foreground bg-background flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>ECHOE</h1>
+      <ThemeSwitcher />
+>>>>>>> Stashed changes
     </main>
   );
 }
--- app/providers.tsx
+++ app/providers.tsx
@@ -1,8 +1,22 @@
 'use client'
 
+<<<<<<< Updated upstream
 import { ThemeProvider } from "next-themes"
 import React from "react"
 
 export function Providers({ children }: {children: React.ReactNode }){
     return <ThemeProvider attribute="class" defaultTheme="system" enableSystem>{children}</ThemeProvider>
+=======
+import {NextUIProvider} from '@nextui-org/react'
+import {ThemeProvider as NextThemesProvider} from "next-themes";
+
+export function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        {children}
+      </NextThemesProvider>
+    </NextUIProvider>
+  )
+>>>>>>> Stashed changes
 }
